a8fe5c8782bf4e07ec8d36353948f111b397cf0f
fei924926 <360714691@qq.com>
2018-03-12 14:42:06 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/test/Test.java
@@ -0,0 +1,10 @@
+package cn.bdqn.test;
+
+/**
+ * Created by lenovo on 2018/3/12.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
